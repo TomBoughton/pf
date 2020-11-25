121e40bdcd4daf93c88d91bbd6d45c5716743caf
TomBoughton <72128179+TomBoughton@users.noreply.github.com>
2020-11-25 16:23:55 +0000

    aa

---
+++ New Unity Project/Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] Transform cam;
+    [SerializeField] float sensitivity;
+    float headRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+    float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
+    transform.Rotate(0f, x, 0f);
+    headRotation += y;
+    cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
+    }
+}
